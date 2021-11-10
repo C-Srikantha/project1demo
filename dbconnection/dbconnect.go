f082fd17064a1daeccaf07aa0bd5c7fe3fbc1c56
cs <cs@gmail.com>
2021-11-10 17:51:45 +0530

    dbconnection

---
+++ dbconnection/dbconnect.go
@@ -0,0 +1,21 @@
+package dbconnection
+
+import (
+	"context"
+
+	"github.com/go-pg/pg"
+)
+
+//connection to the database
+func DatabaseConnection() (*pg.DB, error) {
+	dbdetail := &pg.Options{
+		User:     "postgres",
+		Password: "codecraft",
+		Addr:     ":8080",
+		Database: "project1demo",
+	}
+	connection := pg.Connect(dbdetail)
+	control := context.Background()
+	_, err := connection.ExecContext(control, "SELECT 1")
+	return connection, err
+}
