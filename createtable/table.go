9f2fe01ddadb2ed2fd54a3c9e6fb4a071ae83324
cs <cs@gmail.com>
2021-11-10 17:51:17 +0530

    createtable

---
+++ createtable/table.go
@@ -0,0 +1,33 @@
+package createtable
+
+import (
+	"github.com/go-pg/pg"
+	"github.com/go-pg/pg/orm"
+)
+
+//defines the tabel feilds using struct
+type Registration struct {
+	Id        int `pg:",pk"`
+	Firstname string
+	Lastname  string
+	Username  string `sql:",unique"`
+	Password  string
+	Email     string `sql:",unique"`
+}
+
+//Creates tabel in database and returns error
+func Createtable(db *pg.DB) error {
+	table := []interface{}{
+		(*Registration)(nil),
+	}
+	for _, table := range table {
+		err := db.Model(table).CreateTable(&orm.CreateTableOptions{ //query for creating tabel
+			IfNotExists: true, //checks if table exists or not
+			Varchar:     50,   //converts all string type to varchar in db
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
