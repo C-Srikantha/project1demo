03455b0d5e61edba12f568f0c5ea50cf13bee49e
cs <cs@gmail.com>
2021-11-11 17:56:42 +0530

    added handlers

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/go-pg/pg"
+	"github.com/gorilla/mux"
 	"project1.com/project/createtable"
 	"project1.com/project/dbconnection"
+	"project1.com/project/endpoints"
 )
 
 //prints the errors to the user end
@@ -16,7 +19,13 @@ func error(err interface{}) {
 		os.Exit(1)
 	}
 }
+
 func handlerequest(db *pg.DB) {
+	mux := mux.NewRouter()
+	mux.HandleFunc("/registration", func(rw http.ResponseWriter, r *http.Request) { endpoints.PostRegistration(rw, r, db) }).Methods("POST")
+	mux.HandleFunc("/login", func(rw http.ResponseWriter, r *http.Request) { endpoints.Login(rw, r, db) }).Methods("POST")
+	err := http.ListenAndServe(":8081", mux)
+	error(err)
 
 }
 func main() {
